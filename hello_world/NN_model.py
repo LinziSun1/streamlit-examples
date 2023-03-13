21dd6db9ebd09b37ccd135e11fe18d2d54ab7530
Linzi <linzisun1999@gmail.com>
2023-03-12 21:51:14 -0400

    test

--- hello_world/NN_model.py
+++ hello_world/NN_model.py
@@ -2,12 +2,22 @@ import pandas as pd
 import numpy as np
 import pickle
 import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 
 # loading in the model to predict on the data
-pickle_in = open('classifier.pkl', 'rb')
-classifier = pickle.load(pickle_in)
+#pickle_in = open('classifier.pkl', 'rb')
+#classifier = pickle.load(pickle_in)
 
+df = pd.read_csv('Iris.csv')
+df.drop('Id', axis = 1, inplace = True)
+df['Species']= df['Species'].map({'Iris-setosa':0, 'Iris-versicolor':1, 'Iris-virginica':2})
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+classifier = RandomForestClassifier()
+classifier.fit(X_train, y_train)
 
 def welcome():
     return 'welcome all'
@@ -16,6 +26,7 @@ def welcome():
 # defining the function which will make the prediction using
 # the data which the user inputs
 def prediction(sepal_length, sepal_width, petal_length, petal_width):
+
     prediction = classifier.predict(
         [[sepal_length, sepal_width, petal_length, petal_width]])
     map = {0:'Iris-setosa', 1:'Iris-versicolor', 2:'Iris-virginica'}
